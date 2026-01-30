96c6ac1b8d615afc6bb5d6c0fa7277bd9ff200f9
Rishabjain13 <rishabjain87723@gmail.com>
2026-01-30 16:33:32 +0530

    Add role field to User model

--- app/models/user.py
+++ app/models/user.py
@@ -8,3 +8,5 @@ class User(Base):
     name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
+    role = Column(String, default="user")
+
