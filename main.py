f85698c6d1c3615e8585706d1d757e0db748c59b
Rishabjain13 <rishabjain87723@gmail.com>
2026-02-02 16:33:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,6 @@ from app.routes.auth_routes import router as auth_router
 from app.routes.todo_routes import router as todo_router
 from app.routes.admin_routes import router as admin_router
 
-
-
-
-
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="TodoApp")
