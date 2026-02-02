763f890ceecfe11000c82d232af10796690bbee4
Rishabjain13 <rishabjain87723@gmail.com>
2026-02-02 17:03:47 +0530

    Update todo.py

--- app/models/todo.py
+++ app/models/todo.py
@@ -4,8 +4,10 @@ from app.database import Base
 class Todo(Base):
     __tablename__ = "todos"
 
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    priority = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    priority = Column(String, nullable=False)
     completed = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey("users.id"), index=True)
+
+    is_deleted = Column(Boolean, default=False)
