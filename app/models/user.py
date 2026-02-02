765a745de77608ccecc70da942178f7a1cdf5cec
Rishabjain13 <rishabjain87723@gmail.com>
2026-02-02 17:16:16 +0530

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -4,9 +4,8 @@ from app.database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     role = Column(String, default="user")
-
