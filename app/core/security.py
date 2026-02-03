a842ab2bc8415b92984aaba3e6e617b8d17a581d
Rishabjain13 <rishabjain87723@gmail.com>
2026-02-03 12:41:34 +0530

    Update security.py

--- app/core/security.py
+++ app/core/security.py
@@ -8,8 +8,14 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 
-ACCESS_TOKEN_EXPIRE_MINUTES = 15
-REFRESH_TOKEN_EXPIRE_DAYS = 7
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
+)
+
+REFRESH_TOKEN_EXPIRE_DAYS = int(
+    os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7)
+)
+
 
 if not SECRET_KEY:
     raise RuntimeError("SECRET_KEY is not set in environment variables")
@@ -21,7 +27,7 @@ def create_access_token(user_id: int, role: str):
         "role": role,
         "type": "access",
         "exp": datetime.now(timezone.utc)
-        + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
+        + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     }
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
@@ -32,7 +38,7 @@ def create_refresh_token(user_id: int, role: str):
         "role": role,
         "type": "refresh",
         "exp": datetime.now(timezone.utc)
-        + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS),
+        + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
     }
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
